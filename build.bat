a020ed54bb33de79ab2f3a7bd9c7de7185239b60
marvinO1 <marvinO1@users.noreply.github.com>
2019-03-10 14:54:15 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,8 +1,13 @@
+:: Simple build pipeline to produce docker image local.
+
+:: Build jar and docker image
 call mvn clean install
 call docker image build -f Dockerfile -t marvino1/hello-app .
+
+:: Upload to docker hub
 call docker login
 call docker push marvino1/hello-app:latest
+
+:: Inform openshift about the changed image ...
 call oc.exe login https://api.starter-us-east-1.openshift.com --token=LdEwsx2YiQVkq7sqdLnXfcMSI3-Y23vVuLhUgkBqV0k
 call oc.exe import-image hello-app --from=marvino1/hello-app:latest --confirm
-
-  
