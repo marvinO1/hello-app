596d12e291745eb44d6c3e131aedcfbff3731b21
Administrator <admin@example.com>
2019-03-12 19:40:34 +0100

    add some meaning less comment

--- src/main/java/hello/HossaException.java
+++ src/main/java/hello/HossaException.java
@@ -1,7 +1,7 @@
 package hello;
 
 public class HossaException extends RuntimeException {
-
+    // todo: some improvements ...
     public HossaException(String message) {
         super(message);
     }
